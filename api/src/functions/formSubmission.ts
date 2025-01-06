226672db3244127f13d1cb9ff1932065e29ed2cb
Greg Whatley <me@gregwhatley.dev>
2025-01-06 12:22:14 -0600

    Fix reCAPTCHA

--- api/src/functions/formSubmission.ts
+++ api/src/functions/formSubmission.ts
@@ -1,5 +1,5 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import recaptcha from 'recaptcha-promise'
+import { RecaptchaEnterpriseServiceClient } from '@google-cloud/recaptcha-enterprise'
 import { createTransport } from 'nodemailer'
 import sanitizeHtml = require("sanitize-html");
 
@@ -105,7 +105,7 @@ export async function formSubmission(request: HttpRequest, context: InvocationCo
 
     // Environment variables
     const {
-        RECAPTCHA_SECRET,
+        GCLOUD_API_KEY,
         EMAIL_FROM,
         EMAIL_PASSWORD,
         EMAIL_TO_MAIN,
@@ -114,22 +114,25 @@ export async function formSubmission(request: HttpRequest, context: InvocationCo
         SMTP_PORT
     } = process.env
     // Return code 500 if one or more are falsy
-    let invalidEnvironment = [RECAPTCHA_SECRET, EMAIL_FROM, EMAIL_PASSWORD, EMAIL_TO_MAIN, EMAIL_TO_PROCESS, SMTP_SERVER, SMTP_PORT].some(value => !value)
+    let invalidEnvironment = [GCLOUD_API_KEY, EMAIL_FROM, EMAIL_PASSWORD, EMAIL_TO_MAIN, EMAIL_TO_PROCESS, SMTP_SERVER, SMTP_PORT].some(value => !value)
     if (invalidEnvironment) {
         context.error('Invalid server configuration: one or more required environment variables are unset.')
         return { jsonBody: { message: 'Invalid server configuration.' }, status: 500 }
     }
 
     // Verify reCAPTCHA token
-    const token = formData.get('g-recaptcha-response')?.toString()
-    const verifyCaptcha = recaptcha.create({ secret: RECAPTCHA_SECRET })
-    const challengePassed = await verifyCaptcha(token)
-    if (!challengePassed) {
-        context.info('reCAPTCHA verification failed.', token)
-        return { jsonBody: { message: 'reCAPTCHA verification failed.' }, status: 400 }
+    try {
+        const token = formData.get('g-recaptcha-response')?.toString()
+        const score = await createAssessment({token, apiKey: GCLOUD_API_KEY}, context)
+        if (score <= 0.7) {
+            context.info('reCAPTCHA verification failed.', token, score)
+            return { jsonBody: { message: 'reCAPTCHA verification failed.' }, status: 400 }
+        }
+    } catch (err) {
+        context.error('reCAPTCHA setup failure', err)
+        return { jsonBody: { message: 'reCAPTCHA verification failed.' }, status: 500 }
     }
     
-    // TODO
     // Set up email transport
     const transport = createTransport({
         host: SMTP_SERVER,
@@ -182,3 +185,60 @@ app.http('formSubmission', {
     authLevel: 'anonymous',
     handler: formSubmission
 });
+
+/**
+  * Create an assessment to analyze the risk of a UI action.
+  *
+  * projectID: Your Google Cloud Project ID.
+  * recaptchaSiteKey: The reCAPTCHA key associated with the site/app
+  * token: The generated token obtained from the client.
+  * recaptchaAction: Action name corresponding to the token.
+  */
+async function createAssessment({
+    // TODO: Replace the token and reCAPTCHA action variables before running the sample.
+    projectID = "on-time-couriers-1736186021556",
+    recaptchaKey = "6Lfh668qAAAAANPpXJ15F8nr6KG1CbSiPRR6NDYO",
+    apiKey = "api-key",
+    token = "action-token",
+    recaptchaAction = "submit",
+}, context: InvocationContext) {
+    // Create the reCAPTCHA client.
+    // TODO: Cache the client generation code (recommended) or call client.close() before exiting the method.
+    const client = new RecaptchaEnterpriseServiceClient({apiKey});
+    const projectPath = client.projectPath(projectID);
+
+    // Build the assessment request.
+    const request = ({
+        assessment: {
+            event: {
+                token: token,
+                siteKey: recaptchaKey,
+            },
+        },
+        parent: projectPath,
+    });
+
+    const [response] = await client.createAssessment(request);
+    // Check if the token is valid.
+    if (!response.tokenProperties.valid) {
+        context.error(`The CreateAssessment call failed because the token was: ${response.tokenProperties.invalidReason}`);
+        await client.close()
+        return 0;
+    }
+
+    // Check if the expected action was executed.
+    // The `action` property is set by user client in the grecaptcha.enterprise.execute() method.
+    if (response.tokenProperties.action === recaptchaAction) {
+        // Get the risk score and the reason(s).
+        // For more information on interpreting the assessment, see:
+        // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+        context.info(`The reCAPTCHA score is: ${response.riskAnalysis.score}, because: ${response.riskAnalysis.reasons?.join(', ') ?? ''}`);
+        await client.close()
+
+        return response.riskAnalysis.score;
+    } else {
+        context.error("The action attribute in your reCAPTCHA tag does not match the action you are expecting to score");
+        await client.close()
+        return 0;
+    }
+}
